3ddefe185f53b9288f4698c6d96c4fa13add4701
Kássio Silva <kassio.slva@gmail.com>
2019-02-28 22:20:50 -0300

    Componente Header finalizado

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+import '../styles/styles.scss';
+
+export default class App extends Component {
+  render() {
+    // const { titulo } = this.state;
+
+    return <Header />;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-import Header from './components/Header';
-
-import './styles/styles.scss';
-
-class App extends Component {
-  render() {
-    // const { titulo } = this.state;
-
-    return <Header />;
-  }
-}
+import App from './components/App';
 
 render(<App />, document.getElementById('app'));
